d988bd6e8648004d129986831345210a5e17b3b5
Inderpal Singh <inderps@thoughtworks.com>
2013-12-16 23:41:21 +0530

    added message modal

--- app/app.js
+++ app/app.js
@@ -6,6 +6,7 @@ $(function(){
 
 var Photonetra = can.Construct.extend({
     init: function() {
+        MessageModal.init();
         $('#menu').sidr({name: 'main-menu', source: '#menu-list', side: 'right'});
         new RoutingCtrl("body");
     }
