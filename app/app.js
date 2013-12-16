1a2d8f4fd7b3d0b92aae195abe977c9442d66ee8
Inderpal Singh <inderps@thoughtworks.com>
2013-12-17 00:07:37 +0530

    added spinner

--- app/app.js
+++ app/app.js
@@ -6,6 +6,7 @@ $(function(){
 
 var Photonetra = can.Construct.extend({
     init: function() {
+        window.photographerId = 4;
         MessageModal.init();
         $('#menu').sidr({name: 'main-menu', source: '#menu-list', side: 'right'});
         new RoutingCtrl("body");
--- app/commons/loader.js
+++ app/commons/loader.js
@@ -2,9 +2,15 @@ var Loader = (function() {
 
     var start = function() {
         $.sidr('close', 'main-menu');
+        var waitLoaderDOM = '<div id="wait-loader-backdrop"><div class="spinner-new"></div></div>';
+        this.stop();
+        $('body').append(waitLoaderDOM);
+        $('body').addClass('no-pointer-events');
     };
 
     var stop = function() {
+        $('body').removeClass('no-pointer-events');
+        $('#wait-loader-backdrop').remove();
     };
 
     return {
