cb3beb3d7e970eed472205cddcd79e8d5832a59e
Inderpal Singh <inderps@thoughtworks.com>
2013-12-23 21:05:57 +0530

    added validation framework, added validation for signup form

--- app/signupCtrl.js
+++ app/signupCtrl.js
@@ -6,15 +6,46 @@ var SignupCtrl = can.Control.extend({
 
         var signupView = can.view("#signup-view");
         this.element.html(signupView);
+
+        Validate.setup("#signup-form", {
+            name: {
+                required: true
+            },
+            studio_name: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            password: {
+                required: true,
+                minlength : 5
+            },
+            password: {
+                required: true,
+                minlength : 5
+            },
+
+            confirm_password: {
+                required: true,
+                minlength : 5,
+                equalTo : "#password"
+            }
+        });
+
         Loader.stop();
     },
 
     ".submit click": function (el, ev) {
         ev.preventDefault();
-        var formData = this.element.find("form").serializeJSON();
-        delete formData["confirm_password"]
-        Photographer.create(formData, function(data){
-            MessageModal.show("Your account is created successfully", "#signup");
-        });
+
+        if(this.element.find("form").valid()){
+            var formData = this.element.find("form").serializeJSON();
+            delete formData["confirm_password"]
+            Photographer.create(formData, function(data){
+                MessageModal.show("Your account is created successfully", "#signup");
+            });
+        }
     }
 });
